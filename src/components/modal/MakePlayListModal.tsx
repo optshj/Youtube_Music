48d7c0fab653b761d07c603332942d4452737a0b
optshj <optshj@gmail.com>
2024-03-08 18:02:52 +0900

    [Feat] 모달창을 react-modal 라이브러리로 구현

--- src/components/modal/MakePlayListModal.tsx
+++ src/components/modal/MakePlayListModal.tsx
@@ -1,32 +1,47 @@
-import React from "react";
-import styled from "styled-components";
 import Modal from 'react-modal';
+import ReactModal from "react-modal";
 
 import { IsModalOpen } from "../context/ModalContext";
+import { useModal } from "../context/ModalContext";
 
 import Title from "./Title";
 import Content from "./Content";
 
 
-const ModalForm = styled.div`
-    display:block;
-    position:fixed;
-    width:560px;
+const ModalStyles:ReactModal.Styles = {
+    overlay: {
+        width: "100%",
+        height: "100%",
+        position: "fixed",
+        top: 0,
+        left: 0,
+        zIndex: 103,
+        backgroundColor: " rgba(0, 0, 0, 0.8)",
+    },
+    content: {
+        display: "block",
+        position: "fixed",
+        width: "560px",
+        height:"400px",
+        border: "1px solid rgba(255,255,255,0.1)",
+        boxShadow: "0 16px 24px 2px rgba(0,0,0,.14), 0 6px 30px 5px rgba(0,0,0,.12), 0 8px 10px -5px rgba(0,0,0,.4)",
+        backgroundColor: "#212121",
+        borderRadius: 3,
+        zIndex: 104,
+        padding:"0",
+        overflow:'hidden'
+    }
+};
 
-    border: 1px solid rgba(255,255,255,0.1);
-    box-shadow:0 16px 24px 2px rgba(0,0,0,.14), 0 6px 30px 5px rgba(0,0,0,.12), 0 8px 10px -5px rgba(0,0,0,.4);
-    background-color:#212121;
-    border-radius:3px;
-`
 function MakePlayListModal(){
     const isOpen = IsModalOpen();
+    const {close} = useModal();
+
     return(
         <>
-            <Modal isOpen={isOpen}>
-                <ModalForm>
-                    <Title></Title>
-                    <Content></Content>
-                </ModalForm>
+            <Modal isOpen={isOpen} onRequestClose={close} style={ModalStyles}> 
+                <Title></Title>
+                <Content></Content>
             </Modal>
         </>
     )
