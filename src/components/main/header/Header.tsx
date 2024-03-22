99b8781b63328b23c78da9fb1b8eff7e9d03dcd8
optshj <optshj@gmail.com>
2024-03-22 16:41:01 +0900

    [Style] main header의 스크롤 디자인 변경

--- src/components/main/header/Header.tsx
+++ src/components/main/header/Header.tsx
@@ -1,24 +1,53 @@
 import React from "react";
 import styled from "styled-components";
-import MainHeader from "./MainHeader";
+import HeaderButton from "./HeaderButton";
 
 const Wrapper = styled.div`
-    box-sizing:border-box;
-    @media (min-width:936px){
-        margin-left:240px;
+    display:flex;
+    margin:0 auto;
+    overflow-x: auto;
+    white-space:nowrap;
+    max-width:min(calc(100vw - 452px),1478px);
+    @media (max-width:1150px) and (min-width:936px){
+        max-width:min(calc(100vw - 112px - 240px - 12px ),1478px);
     }
-    @media (min-width:616px) and (max-width:936px){
-        margin-left:72px;
+    @media (max-width:936px) and (min-width:615px){
+        max-width:min(calc(100vw - 112px - 72px - 12px ),1478px);
     }
+    @media (max-width:615px){
+        max-width:calc(100vw - 32px);
+    }
+    &:hover{
+        &::-webkit-scrollbar-thumb{
+            background-color:#606060;
+        }
+    }
+    & > *:first-child {
+        margin-left: 0;
+    }
+    
+    & > *:last-child {
+        margin-right: 0;
+    }
+    
 `
 
-function Header() {
+function MainHeader(){
     return(
         <>
             <Wrapper>
-                <MainHeader></MainHeader>
+                <HeaderButton name="운동"></HeaderButton>
+                <HeaderButton name="에너지 충전"></HeaderButton>
+                <HeaderButton name="휴식"></HeaderButton>
+                <HeaderButton name="출퇴근길"></HeaderButton>
+                <HeaderButton name="행복한 기분"></HeaderButton>
+                <HeaderButton name="집중"></HeaderButton>
+                <HeaderButton name="로맨스"></HeaderButton>
+                <HeaderButton name="파티"></HeaderButton>
+                <HeaderButton name="슬픔"></HeaderButton>
+                <HeaderButton name="잠잘 때"></HeaderButton>
             </Wrapper>
         </>
     )
 }
-export default Header;
+export default MainHeader;
