f8c74cdd6bc4e39616325b4f57baa65f6f344a46
optshj <optshj@gmail.com>
2024-04-02 11:24:12 +0900

    [Style] Mobile Sidebar 애니메이션 추가

--- src/components/sidebar/mobile/MobileSidebar.tsx
+++ src/components/sidebar/mobile/MobileSidebar.tsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import styled from "styled-components";
 
 import { IsWebSidebarOpen } from "../../context/SidebarContext";
 
 import Menu from "./menu/Menu";
+import { throttle } from "lodash";
 
-const Wrapper = styled.div<{isOpen:boolean}>`
+const Wrapper = styled.div<{isOpen:boolean,isScrollTop:boolean}>`
     margin-top:64px;
     width:72px;
     position:fixed;
     height:100%;
+    border-right:${(props)=> (props.isScrollTop?'0px solid transparent':'1px solid rgba(255,255,255,.15)')};
+    transitio:all 0.2s linear;
     @media (min-width:936px){
-        display:none;
+        display:none;   
     }
     @media (max-width:616px){
         display:none;
@@ -19,10 +22,21 @@ const Wrapper = styled.div<{isOpen:boolean}>`
 `
 function MobileSidebar(){
     const isOpen = IsWebSidebarOpen();
-    
+    const [isScrollTop,setIsScrollTop] = useState(true);
+
+	const ScrollMove = throttle (() =>{
+		const isScrollTop = window.scrollY === 0;
+		setIsScrollTop(isScrollTop);
+	},500)
+
+	useEffect(()=> {
+		window.addEventListener('scroll',ScrollMove);
+
+		return () => window.removeEventListener('scroll',ScrollMove);
+	},[ScrollMove])
     return(
         <>
-            <Wrapper isOpen={isOpen}>
+            <Wrapper isOpen={isOpen} isScrollTop={isScrollTop}>
                 <Menu></Menu>
             </Wrapper>
         </>
